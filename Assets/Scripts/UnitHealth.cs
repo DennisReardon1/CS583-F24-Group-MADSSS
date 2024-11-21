55efe0e37b0e228ea850b687a7d2a5f6cefddae3
DennisReardon1 <dreardon6722@sdsu.edu>
2024-11-20 22:47:32 -0800

    Added Health / HealthUI

---
+++ Assets/Scripts/UnitHealth.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealth
+{
+    private int currHealth;
+    private int maxHealth;
+
+    //getter and setters
+    public int Health{
+        get{
+            return currHealth;
+        }
+        set{
+            currHealth = value;
+        }
+    }//currentHealth
+
+
+    public int MaxyHealth{
+        get{
+            return maxHealth;
+        }
+        set{
+            maxHealth = value;
+        }
+    }//maxHealth
+
+    public UnitHealth(int health1, int maxHealth1){
+        currHealth = health1;
+        maxHealth = maxHealth1;
+    }
+    
+    public void HurtMe(int Damage){
+        if(currHealth > 0){
+            currHealth = currHealth - Damage;
+        }
+    }
+
+
+    public void HealMe(int Healing){
+        if(currHealth < maxHealth){
+            currHealth = currHealth + Healing;
+        }
+        if(currHealth > maxHealth){
+            currHealth = maxHealth;
+        }
+    }
+
+}
